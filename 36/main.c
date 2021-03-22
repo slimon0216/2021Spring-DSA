9b947f262eb427c55e4fcf867eeaadbe1045404c
slimon110 <45115446+slimon110@users.noreply.github.com>
2021-03-22 13:45:39 +0800

    存檔點，之後改壞了可以回來

--- 36/main.c
+++ 36/main.c
@@ -5,31 +5,181 @@
 #include <math.h>
 #include <assert.h>
 #define MAX_LEN 100000
- 
-typedef struct Stack {
-    int top;  // arr index of the top element
-    int capacity;  //
-    int size;  // # of elements
-    int *arr;  // array of elements
-}Stack;
 
-struct Stack *createStack() {
-    struct Stack *S = malloc(sizeof(struct Stack));
-    S->capacity = MAX_LEN;
-    S->top = -1;
-    S->arr = (int*)malloc(S->capacity * sizeof(int));
-    S->size = 0;
-    return S;
-};
+#define true 1
+#define false 0
+typedef int bool;
 
-inline int isFullStack(struct Stack *S);
-inline void push(struct Stack *S, int data);
-inline void pop(struct Stack *S);
-inline int peek(struct Stack *S);
-inline int isEmpty(struct Stack *S);
-inline void clear(struct Stack *S);
+typedef struct Node {
+    int val;
+    struct Node *prev, *next;
 
-inline void print(Stack *s);
+}Node;
+
+Node* createNode(){
+    Node *node = malloc(sizeof(Node));
+    node ->val = 0;
+    node ->prev = NULL;
+    node ->next = NULL;
+    return node;
+}
+
+typedef struct DList{
+    Node *head;
+    Node *tail;
+    int size;
+    bool isReverse;
+}DList;
+
+
+
+DList* createDList(){
+    DList *D = (DList*)malloc(sizeof(DList));
+    D->head = D->tail = NULL;
+    D->size = 0;
+    D->isReverse = false;
+    return D;
+}
+
+void insert(DList* D, int val){
+    Node *node = malloc(sizeof(Node));
+    node->val = val;
+    node->prev = node->next = NULL;
+
+    if (D->size == 0) 
+        D->head = D->tail = node;
+    else
+    {
+        D->tail->next = node;
+        node->prev = D->tail;
+        D->tail = node; 
+    }
+    ++D->size;
+}
+
+
+void message(char* message){
+    printf("%s\n", message);
+}
+void print(DList* D){
+    if (D->size == 0)
+    {
+        printf("\n");
+        return;
+    }
+    Node *ptr = D->head;
+    while (ptr != D->tail)
+    {
+        printf("%d ", ptr->val);
+        ptr = ptr->next;
+        // message("test");
+    }
+    assert(ptr != NULL);
+    // if (ptr != NULL)
+        printf("%d\n", ptr->val);
+    // else printf("\n");
+    
+    // for (int i = 0; i < D->size; ++i)
+    // {
+    //     printf("%d ", ptr->val);
+    //     ptr = ptr->next;
+    //     assert(ptr != NULL);
+    //     if (i == D->size-1)
+    //         printf("\n");
+    // }
+}
+
+
+
+void pop_back(DList* D){
+    if (D->size == 0)
+        return;
+    else if (D->size == 1)  //only one node
+    {
+        free(D->tail);
+    // message("'test'");
+        D->tail = D->head = NULL;
+        D->size = 0;
+        // return;
+    }
+    else    //size > 2
+    {
+        Node *ptr = D->tail;
+        ptr = ptr->prev;    '''prev真的是prev嗎???'''
+
+        ptr->next = NULL;
+        free(D->tail);
+        D->tail = ptr;
+        --D->size;
+    }
+}
+
+void reverse(DList* D){
+    // printf("%d\n", D->size);
+    if (D->size <= 1) // # of node <= 1 do nothing
+        return;
+
+    Node* ptr = D->head;
+    // turn head into tail
+        // printf("%d\n", ptr->val);
+    ptr->prev = ptr->next;
+    ptr->next = NULL;
+    ptr = ptr->prev;
+    Node *temp;
+    // print(D);
+    // message("test");
+    while (ptr != D->tail)
+    {
+        // printf("%d\n", ptr->val);
+        temp = ptr->prev;
+        ptr->prev = ptr->next;
+        ptr->next = temp;
+        ptr = ptr->prev;
+    }
+    //now ptr is tail
+    // printf("sdf %d\n", D->tail);
+
+    // ptr is tail
+    if (ptr != NULL)
+    {
+        ptr->next = ptr->prev;
+        ptr->prev = NULL;
+    }
+    
+    temp = D->head;
+    D->head = D->tail;
+    D->tail = temp;
+    // print(D);
+}
+void migrate(DList* a, DList* b) // ra-th rail is shut down, and all its cabins is migrated into the rb-th
+{
+    if (a->size == 0)  //a has no node
+        return ;
+        // message("text");
+    
+    else if (b->size == 0)   // b has no node
+    {
+        reverse(a);
+        b->head = a->head;
+        b->tail = a->tail;
+        a->tail = a->head = NULL;
+        b->size = a->size;
+        a->size = 0;
+    }
+    else
+    {
+        reverse(a);
+        // print(a);
+        b->tail->next = a->head;
+        b->tail = a->tail;
+        a->head = a->tail = NULL;
+        b->size += a->size;
+        // printf("tail %d\n", b->tail->val);
+        a->size = 0;
+    }
+    assert(b->tail != NULL);
+}
+
 
 char cmd[10];
 int r, l, ra, rb;
@@ -38,52 +188,65 @@ int main()
     int numOfRail_k, numOfRec_n;
     scanf("%d %d", &numOfRail_k, &numOfRec_n);
     // printf("%d %d", numOfRail_k, numOfRec_n);
-    Stack **stack_arr = malloc(sizeof(Stack*) * numOfRail_k);
+    DList **dl = malloc(sizeof(DList*) * numOfRail_k);
     for(int i = 0; i < numOfRail_k; ++i)
     {    
-        // int temp;
-        stack_arr[i] = createStack();
+        int temp;
+        dl[i] = createDList();
+        // while (scanf("%d", &temp) != EOF)
+        //     insert(dl[i], temp);
     }
- 
+    // for(int i = 0; i < numOfRail_k; ++i)
+    // {    
+    //     reverse(dl[i]);
+    //     print(dl[i]);
+    // }
+    // reverse(dl[0]);
+    // migrate(dl[1], dl[0]);    
+    // // print(dl[0]);
+    // print(dl[1]);
+    // pop_back(dl[1]);
+    // // print(dl[0]);
+    // print(dl[1]);
+
     while(numOfRec_n > 0)
     {
         --numOfRec_n;
         scanf("%s", cmd);
         // message(cmd);
-        if (cmd[0] == 'e')  //enter
+        if (strcmp(cmd, "enter") == 0)
         {
             scanf("%d %d", &r, &l);
-            push(stack_arr[r], l);
+            // printf("%d %d\n", r, l);
+            insert(dl[r], l);
         }
-        else if (cmd[0] == 'l') //leave
+        else if (strcmp(cmd, "leave") == 0)
         {
             scanf("%d", &r);
-            pop(stack_arr[r]);
+            // printf("%d\n", r);
+            pop_back(dl[r]);
         }
-        else  //migrate
+        else if (strcmp(cmd, "migrate") == 0)
         {
             scanf("%d %d", &ra, &rb);
-            while(!isEmpty(stack_arr[ra]))
-            {
-                push(stack_arr[rb], peek(stack_arr[ra]));
-                pop(stack_arr[ra]);
-            }
+            // printf("%d %d\n", ra, rb);
+            // reverse(dl[ra]);
+            // migrate(dl[ra], dl[rb]);
+            // message("test");
         }
-            // printf("\n");
         // for(int i = 0; i < numOfRail_k; ++i)
         // {    
-        //     // reverse(stack_arr[i]);
-        //     printf("rail %d: ", i);
-        //     print(stack_arr[i]);
+        //     // reverse(dl[i]);
+        //     // printf("rail %d , size = %d: ", i, dl[i]->size);
+        //     print(dl[i]);
         // // message("tes");
         // }
         //     printf("\n");
-            // printf("\n");
     }
     for(int i = 0; i < numOfRail_k; ++i)
     {    
-        // reverse(stack_arr[i]);
-        print(stack_arr[i]);
+        // reverse(dl[i]);
+        print(dl[i]);
     }
 
     return 0;
@@ -92,16 +255,12 @@ int main()
 
 void push(struct Stack *S, int data) {
     S->arr[++S->top] = data;
-    ++S->size;
     // if (isFullStack(S))
     // enlarge(S);
 };
 
 void pop(struct Stack *S){
-    if (isEmpty(S))
-        return;
     --S->top;
-    --S->size;
 }
 
 int peek(struct Stack *S){
@@ -115,26 +274,11 @@ int isFullStack(struct Stack *S) {
 };
 
 int isEmpty(struct Stack *S){
-    return S->size == 0;
+    return (S->top == -1)? 1 : 0;
 }
 
 void clear(struct Stack *S){
     S->top = -1;
-    S->size = 0;
 }
 
-void print(Stack *s){
-    if (isEmpty(s))
-    {
-        printf("\n");
-        return;
-    }
-    int len = s->size;
-    for (int i = 0; i < len; ++i)
-    {
-        printf("%d ", s->arr[i]);
-        if (i == len-1)
-            printf("\n");
-    }
-    // printf("\n");
-}
+

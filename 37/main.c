0335fc5e0f439e6f486333adf6cac8364c3d7525
slimon110 <45115446+slimon110@users.noreply.github.com>
2021-04-03 04:38:11 +0800

    37  提交測試

--- 37/main.c
+++ 37/main.c
@@ -110,7 +110,7 @@ int main()
     // scanf("%d %d", &n_len_int_seq, &q_num_query);
     n_len_int_seq = readInt();
     q_num_query = readInt();
-    MAX_LEN = 100;
+    MAX_LEN = 300;
     // printf("hello");
     DList *list = createDList();
     list->tail = list->head = createNode();

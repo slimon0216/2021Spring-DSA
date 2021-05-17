3b0cbb9bc8629d14e07dcec9cb6ab4236b630b28
slimon <slimon0216@gmail.com>
2021-05-17 10:59:20 +0800

    44 update

--- 44/main.c
+++ 44/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <assert.h>
 #define MAX 100000
 #define PUSH 0
 #define MERGE 1
 int sc ;
 typedef struct Node
 {
-    int value;
+    int value, prod_id;
     struct Node *next;
     struct Node *prev;
 } Node;
@@ -15,6 +15,7 @@ typedef struct LNode
 {
     int value, dist;
     struct LNode *right, *left, *parent;
+    Node* copy;
 } LNode;
 
 typedef struct List
@@ -26,18 +27,20 @@ typedef struct List
 
 Node *list_node[MAX] = {NULL};
 LNode *heap_node[MAX] = {NULL};
+// int which_list[MAX] = {0};
 
 Node *createNode(int val)
 {
     Node *node = malloc(sizeof(Node));
-    node->value = val;
+    node->value = node->prod_id = val;
     node->next = node->prev = NULL;
     return node;
 }
 
-void insert_list(List *list, int val)
+void insert_list(List *list, int val, int line_index)
 {
     Node *node = createNode(val);
+    node->prod_id = line_index;
     list_node[val] = node;
     if (list->head == NULL)
         list->head = list->tail = node;
@@ -49,25 +52,38 @@ void insert_list(List *list, int val)
     }
 }
 
-void print_list(List *l)
+void print_list(List *l, int mode)
 {
-    Node *node = l->head;
-    while (node != NULL)
+    if (mode == 1)
     {
-        printf("%d ", node->value);
-        node = node->next;
+        Node *node = l->head;
+        while (node != NULL)
+        {
+            printf("%d ", node->value);
+            node = node->next;
+        }
+        printf("\n");
     }
-    printf("\n");
+    else
+    {
+        Node *node = l->tail;
+        while (node != NULL)
+        {
+            printf("%d ", node->value);
+            node = node->prev;
+        }
+        printf("\n");
+    }
 }
 
-LNode *create_LNode(int val)
+LNode *create_LNode(int val, Node *copy)
 {
     LNode *node;
     node = (LNode *)malloc(sizeof(LNode));
     node->right = node->left = node->parent = NULL;
+    node->copy = copy;
     node->dist = 0;
     node->value = val;
-
     return node;
 }
 
@@ -103,9 +119,9 @@ LNode *merge_lheap(LNode *a, LNode *b)
         a->dist = 1 + (a->right->dist);
     return a;
 }
-LNode *insert_lheap(LNode *root, int val)
+LNode *insert_lheap(LNode *root, int val, Node *copy)
 {
-    LNode *node = create_LNode(val);
+    LNode *node = create_LNode(val, copy);
     heap_node[val] = node;
     root = merge_lheap(root, node);
     return root;
@@ -123,12 +139,11 @@ int ReadInt();
 
 int operations[MAX * 2 + 2][3];
 int target_line[MAX] = {0};
-int status[MAX] = {0};  // -1 已經pop, 0 pop不出來, 1 代表頭尾 , 2代表最高, 3代表 1 and 2
+int status[MAX] = {0};  // -2 已經pop, -1 存在但pop不出來, 0 還沒進來, 1 代表頭尾 , 2代表最高, 3代表 1 and 2
 List *prod_lines[MAX] = {NULL};
 LNode *heap[MAX] = {NULL};
-
-// int freeHeap[MAX] = {0};
-// int a[MAX][MAX];
+int disjoint_set_merged[MAX] = {0};
+// int disjoint_set_origin[MAX] = {0};
 
 int main()
 {
@@ -148,6 +163,8 @@ int main()
         {
             prod_lines[i] = malloc(sizeof(List));
             prod_lines[i]->head = prod_lines[i]->tail = NULL;
+            disjoint_set_merged[i] = i;
+            // disjoint_set_origin[i] = i;
         }
         for (int i = 0; i < num_of_operations; ++i)
         {
@@ -161,7 +178,10 @@ int main()
             operations[i][2] = b;
         }
         for (int i = 0; i < num_of_packages; i++)
+        {    
             target_line[i] = ReadInt();
+            // which_list[i] = -2;
+        }
         
         int tar_index = 0;
         for(int op_index = 0; op_index < num_of_operations; ++op_index)
@@ -170,31 +190,98 @@ int main()
             {
                 int target = target_line[tar_index];
                 int height = operations[op_index][1];
+                // 如果進來的包裹跟 target 一樣就直接 pop，也不用真的 push 進來
                 if (height == target)
                 {   
-                    status[height] = -1;
+                    status[height] = -2;
                     ++tar_index;
                     continue;
                 }
-
-                if (status[target] == 1)
+                //包裹不一樣 就去檢查在不在裡面
+                else if (status[target] == -1)
                 {
+                    printf("impossible\n");
+                    break;
+                }
+                else if (status[target] == 1)  // 在頭尾 可以 pop
+                {
+                    // 找到 target 所在的生產線 pid
+                    int pid = list_node[target]->prod_id;
+                    while (disjoint_set_merged[pid] != pid)
+                        pid = disjoint_set_merged[pid];
+
+                    // 把 target 從生產線 pop掉
+                    if (prod_lines[pid]->head == prod_lines[pid]->tail)
+                        prod_lines[pid]->head = prod_lines[pid]->tail = NULL;
+                    else if (prod_lines[pid]->head->value == target)
+                    {    
+                        prod_lines[pid]->head = prod_lines[pid]->head->next;
+                        if (status[prod_lines[pid]->head->value] == 0)
+                            status[prod_lines[pid]->head->value] = 1;
+                        else if (status[prod_lines[pid]->head->value] == 2)
+                            status[prod_lines[pid]->head->value] = 3;
+                    }
+                    else if (prod_lines[pid]->tail->value == target)
+                    {    
+                        prod_lines[pid]->tail = prod_lines[pid]->tail->next;
+                        if (status[prod_lines[pid]->tail->value] == 0)
+                            status[prod_lines[pid]->tail->value] = 1;
+                        else if (status[prod_lines[pid]->tail->value] == 2)
+                            status[prod_lines[pid]->tail->value] = 3;
+                    }
+                    status[target] = -2;
+                    if (prod_lines[pid]->head != NULL)
+                        status[prod_lines[pid]->head->value] = 1;
+
+                    // 去 heap 裡面刪掉
+                    LNode* prnt = heap_node[height]->parent;
+                    heap_node[height] = pop(heap_node[height]);
+                    if (distance(prnt->right) > distance(prnt->left))
+                    {
+                        LNode *temp = prnt->right;
+                        prnt->right = prnt->left;
+                        prnt->left = temp;
+                    }
+                }
+                else if (status[target] == 2) //最高 可以 pop
+                {
+                    // 找到 target 所在的生產線 id
+                    int pid = list_node[target]->prod_id;
+                    while (disjoint_set_merged[pid] != pid)
+                        pid = disjoint_set_merged[pid];
+
+                    // 先去生產線 pop 掉
+                    Node *to_delete = heap[pid]->copy;
+                    Node *ptr = to_delete->prev;
+                    assert(ptr != NULL);
+                    to_delete->prev->next = to_delete->next;
+                    // to_delete->next->prev = ptr;
+                    
+                    // heap[pid] = pop(heap[pid]);
+                    // status[to_delete->value] = -2;
                     
                 }
-                else if (status[target] == 2)
-                {}
                 else if (status[target] == 3)
-                {}
+                {
+                    // 找到 target 所在的生產線 id
+                    int pid = list_node[target]->prod_id;
+                    while (disjoint_set_merged[pid] != pid)
+                        pid = disjoint_set_merged[pid];
+                }
 
                 int line_index = operations[op_index][2];
                 //push 進去
                 if (prod_lines[line_index]->tail != NULL)
                     if (status[prod_lines[line_index]->tail->value] == 1)
                         status[prod_lines[line_index]->tail->value] = 0;
+                    else if (status[prod_lines[line_index]->tail->value] == 3)
+                        status[prod_lines[line_index]->tail->value] = 2;
+
                 status[height] = 1;
-                insert_list(prod_lines[line_index], height);
-                
-                // heap
+                insert_list(prod_lines[line_index], height, line_index);
+                // which_list[height] = line_index;
+
+                // 處理 heap
                 if (heap[line_index] != NULL)
                     if (status[heap[line_index]->value] == 2)   //heap 的root只該是 2 or 3
                         status[heap[line_index]->value] = 0;
@@ -203,10 +290,10 @@ int main()
 
                 if (heap[line_index] == NULL)
                 {
-                    heap[line_index] = create_LNode(height);
+                    heap[line_index] = create_LNode(height, prod_lines[line_index]->tail);
                 }
                 else
-                    heap[line_index] = insert_lheap(heap[line_index], height);
+                    heap[line_index] = insert_lheap(heap[line_index], height, prod_lines[line_index]->tail);
                 if (status[heap[line_index]->value] == 1)
                     status[heap[line_index]->value] = 3;
                 else if (status[heap[line_index]->value] == 0)
@@ -219,6 +306,13 @@ int main()
             {
                 int broken = operations[op_index][1];
                 int destination = operations[op_index][2];
+                if (broken == 8)
+                {
+                    ;
+                }
+                
+                disjoint_set_merged[broken] = destination;
+
                 if (prod_lines[destination]->head == NULL)
                 {
                     if (prod_lines[broken]->head != NULL)
@@ -250,7 +344,7 @@ int main()
                     }
                 }
                 prod_lines[broken]->head = prod_lines[broken]->tail = NULL;
-                
+
                 if (heap[destination] != NULL || heap[broken] != NULL)
                 {
                     if (heap[destination] != NULL )
@@ -280,7 +374,7 @@ int main()
         for (int i = 0; i < num_of_lines; i++)
         {
             printf("line %d:", i);
-            print_list(prod_lines[i]);
+            print_list(prod_lines[i], 1);
         }
         for (int i = 0; i < num_of_lines; i++)
             heap[i] = NULL;
